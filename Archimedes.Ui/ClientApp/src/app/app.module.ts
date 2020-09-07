9c560423f8d777066cd8c2142448c7f32ec215b0
Simon Burton <burt76@outlook.com>
2020-09-07 22:05:20 +0100

    change to routing

--- Archimedes.Ui/ClientApp/src/app/app.module.ts
+++ Archimedes.Ui/ClientApp/src/app/app.module.ts
@@ -2,21 +2,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule , APP_INITIALIZER} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
-
-import { ConfigurationService } from "./services/configuration.service";
+import { RouterModule, Routes } from '@angular/router';
+import { ConfigurationService } from './services/configuration.service';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
 import { CounterComponent } from './counter/counter.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
-import { FetchValueDataComponent } from "./fetch-value-data/fetch-value-data.component";
+import { FetchValueDataComponent } from './fetch-value-data/fetch-value-data.component';
+
+import {LocationStrategy, HashLocationStrategy} from '@angular/common';
 
 const appInitializerFn = (appConfig: ConfigurationService) => {
   return () => {
     return appConfig.loadConfig();
   };
 };
+
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent, pathMatch: 'full' },
+  { path: 'counter', component: CounterComponent },
+  { path: 'fetch-data', component: FetchDataComponent },
+  { path: 'fetch-value-data', component: FetchValueDataComponent }
+
+]
+
 
 @NgModule({
   declarations: [
@@ -31,12 +42,14 @@ const appInitializerFn = (appConfig: ConfigurationService) => {
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
     HttpClientModule,
     FormsModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
-      { path: 'fetch-value-data', component: FetchValueDataComponent }
-    ])
+    RouterModule.forRoot(appRoutes)
+
+    //  RouterModule.forRoot([
+    //   { path: '', component: HomeComponent, pathMatch: 'full' },
+    //   { path: 'counter', component: CounterComponent },
+    //   { path: 'fetch-data', component: FetchDataComponent },
+    //   { path: 'fetch-value-data', component: FetchValueDataComponent }
+    // ]) 
   ],
   providers: [ConfigurationService,
   {
